4c9f35588cd36479286ab33a18a5421ec2c4bf9e
Safar Umarov <safarumarov711@gmail.com>
2024-06-01 18:49:07 +0700

    initial project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Images host</title>
+    </head>
+    <body>
+        <div>Welcome</div>
+    </body>
+</html>
